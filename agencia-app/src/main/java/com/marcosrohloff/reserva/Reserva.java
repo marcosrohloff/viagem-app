1dc04f8d5a06fa200632a32ba64d2b18dcda131a
Marcos Fernando <marcos.fersh@gmail.com>
2022-10-12 21:21:06 -0300

    Agencia-app - Criacao dos resources clientes e reserva

---
+++ agencia-app/src/main/java/com/marcosrohloff/reserva/Reserva.java
@@ -0,0 +1,15 @@
+package com.marcosrohloff.reserva;
+
+import com.marcosrohloff.cliente.Cliente;
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Reserva extends PanacheEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    public Cliente cliente;
+}
